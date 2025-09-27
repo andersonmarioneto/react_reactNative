8d70efbfeded2db2a462da2662ab13d3f40e8461
andersonmarioneto <netoanderson192@gmail.com>
2025-09-27 14:24:30 +0100

    plusplus

---
+++ anderson/src/components/home/Data.jsx
@@ -0,0 +1,20 @@
+function Data() {
+    return (
+        <>
+            <div className="home__data">
+                <h1 className="home__title">
+                    Mário Neto🖖
+                </h1>
+                <h3 className="home__subtitle">UI Designer - Dev FrontEnd</h3>
+                <p className="home__description">Hello, I'm Anderson Mário Neto, I work with User Interface Design and Web Development and I'm a Computer Science student.</p>
+
+                <a href="#contact" className="button button--flex">
+                    Say Hello &nbsp;
+                    <i class="uil uil-location-arrow"></i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Data
---
+++ anderson/src/components/home/Social.jsx
@@ -0,0 +1,21 @@
+function Social() {
+    return(
+        <>
+            <div className="home__social">
+                <a href="https://www.linkedin.com/in/anderson-m%C3%A1rio-neto-21b56a238/" className="home__social-icon" target="_blank">
+                    <i className="uil uil-linkedin-alt"></i>
+                </a>
+                
+                <a href="https://www.github.com/andersonmarioneto/" className="home__social-icon" target="_blank">
+                    <i className="uil uil-github-alt"></i>
+                </a>
+                
+                <a href="https://www.instagram.com/andersom_mario_neto/" className="home__social-icon" target="_blank">
+                    <i className="uil uil-instagram"></i>
+                </a>
+            </div>
+        </>
+    )
+}
+
+export default Social
